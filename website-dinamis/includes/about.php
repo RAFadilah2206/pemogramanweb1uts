fc18e6351c8786b14782f3f2b310b938938af816
RAFadilah2206 <169122328+RAFadilah2206@users.noreply.github.com>
2024-11-08 21:41:20 +0700

    Add files via upload

---
+++ website-dinamis/includes/about.php
@@ -0,0 +1,12 @@
+<div class="jumbotron">
+	<h5>Hello there!</h5>
+</div>
+
+<div class="container" id="main-content">
+	<p>
+		tugas ini dibuat untuk memenuhi syarat UTS yang diberikan oleh dosen Mata Kuliah Pemograman Web 1, Bapak Eko Budiarto, S.Kom., M.M. Selain itu menjadi langkah awal untuk mengasah kemampuan kami dalam membuat website. </p>
+     <p>
+        Saya menyampaikan terima kasih kepada Bapak Eko Budiarto, S.Kom., M.M. yang telah memberikan  bimbingan sehingga tugas ini dapat di selesaikan. Saya memohon maaf jika masih ada kekurangan dalam pengerjaan Website Dinamis ini. Terima kasih.
+       </p>
+	
+</div>
---
+++ website-dinamis/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap demo</title>
+<!-- Bootstrap -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+<!-- Datatables -->
+<link href="assets/datatables/dataTables.dataTables.css" rel="stylesheet">
+<style>
+        body {
+            background-color: #F08080;
+        }
+    </style>
+
+</head>
+<body>
+    <header>
+        <h1 class="text-center p3">Membuat website dinamis</h1>
+    </header>
+
+<!-- menu -->
+<?php include "includes/navbar.php" ?>
+<!-- ./menu -->
+
+<!-- konten -->
+<div class="container mt-3">
+    <?php include "konten.php" ?>
+</div>
+<!-- ./konten -->
+
+<footer>
+    <div class="text-bg-danger p-3 text-center mt-5">
+        Pemrograman Web 1 @ <?= date("Y") ?>
+    </div>
+</footer>
+
+
+<script src="assets/jquery-3.7.1.js"></script>
+<script src="assets/datatables/dataTables.js"></script>
+
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+<script>
+new DataTable('#example');
+</script>
+
+</body>
+
+</html>
